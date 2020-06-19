69290c1cf4ec86a37d6e4fd9ccc33edd1561dc2d
Appleby <alexappleby70@gmail.com>
2020-06-19 01:17:43 -0600

    added counter to WayPoint

--- src/edu/ahs/robotics/pathfinder/util/WayPoint.java
+++ src/edu/ahs/robotics/pathfinder/util/WayPoint.java
@@ -1,8 +1,11 @@
 package edu.ahs.robotics.pathfinder.util;
 
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 /**
  * A desired robot state on the field, as an element of an autonomous path.
@@ -14,18 +17,38 @@ public class WayPoint {
 
     private Coordinate coordinate;
     private double heading;
+
+    private Group group;
+    private Text label;
     private Circle circle;
 
+    private static int count; //todo integrate count with higher level collection
+    private static final double X_OFFSET = 4.0;
+    private static final Color COLOR = Color.rgb(253,238,0);
+
+
     public WayPoint(Coordinate coordinate, double heading) {
+        count++;
+
         this.coordinate = coordinate;
         this.heading = heading;
 
         circle = new Circle(coordinate.getPixelX(), coordinate.getPixelY(), GRAPHIC_RADIUS);
-        circle.setFill(Color.rgb(253,238,0));
+        circle.setFill(COLOR);
+
+        label = new Text(String.valueOf(count));
+
+        label.setX(coordinate.getPixelX() + X_OFFSET);
+        label.setY(coordinate.getPixelY());
+        label.setFill(COLOR);
+
+        group = new Group();
+
+        group.getChildren().addAll(circle, label);
     }
 
-    public Circle getGraphic(){
-        return circle;
+    public Node getGraphic(){
+        return group;
     }
 
 }
