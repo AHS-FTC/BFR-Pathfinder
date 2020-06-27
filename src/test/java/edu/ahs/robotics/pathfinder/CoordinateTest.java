c9021ee5a99f9771b19bebf2efcf17181a511d5b
Appleby <alexappleby70@gmail.com>
2020-06-27 00:11:39 -0600

    changed test to static import

--- src/test/java/edu/ahs/robotics/pathfinder/CoordinateTest.java
+++ src/test/java/edu/ahs/robotics/pathfinder/CoordinateTest.java
@@ -3,6 +3,7 @@ package edu.ahs.robotics.pathfinder;
 import edu.ahs.robotics.pathfinder.util.Coordinate;
 import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CoordinateTest {
 
@@ -13,8 +14,8 @@ public class CoordinateTest {
         //the top left corner should always be (-72, 72) in inches
         Coordinate c = Coordinate.newFromPixels(0,0);
 
-        Assert.assertEquals(c.getInchX(), -72.0, 0.00001);
-        Assert.assertEquals(c.getInchY(), 72.0, 0.00001);
+        assertEquals(c.getInchX(), -72.0, 0.00001);
+        assertEquals(c.getInchY(), 72.0, 0.00001);
 
     }
 
@@ -24,8 +25,8 @@ public class CoordinateTest {
 
         Coordinate c = Coordinate.newFromPixels(144,144);
 
-        Assert.assertEquals(c.getInchX(), 72, 0.00001);
-        Assert.assertEquals(c.getInchY(), -72, 0.00001);
+        assertEquals(c.getInchX(), 72, 0.00001);
+        assertEquals(c.getInchY(), -72, 0.00001);
 
     }
 
@@ -35,8 +36,8 @@ public class CoordinateTest {
 
         Coordinate c = Coordinate.newFromPixels(75,80);
 
-        Assert.assertEquals(c.getInchX(), 3, 0.00001);
-        Assert.assertEquals(c.getInchY(), -8, 0.00001);
+        assertEquals(c.getInchX(), 3, 0.00001);
+        assertEquals(c.getInchY(), -8, 0.00001);
 
     }
 
@@ -47,8 +48,8 @@ public class CoordinateTest {
         //the top left corner should always be (-72, 72) in inches
         Coordinate c = Coordinate.newFromInches(-72,72);
 
-        Assert.assertEquals(c.getPixelX(), 0, 0.00001);
-        Assert.assertEquals(c.getPixelY(), 0, 0.00001);
+        assertEquals(c.getPixelX(), 0, 0.00001);
+        assertEquals(c.getPixelY(), 0, 0.00001);
 
     }
 
@@ -58,8 +59,8 @@ public class CoordinateTest {
 
         Coordinate c = Coordinate.newFromInches(72,-72);
 
-        Assert.assertEquals(c.getPixelY(), 144, 0.00001);
-        Assert.assertEquals(c.getPixelY(), 144, 0.00001);
+        assertEquals(c.getPixelY(), 144, 0.00001);
+        assertEquals(c.getPixelY(), 144, 0.00001);
 
     }
 
@@ -69,8 +70,8 @@ public class CoordinateTest {
 
         Coordinate c = Coordinate.newFromInches(3,-8);
 
-        Assert.assertEquals(c.getPixelX(), 75, 0.00001);
-        Assert.assertEquals(c.getPixelY(), 80, 0.00001);
+        assertEquals(c.getPixelX(), 75, 0.00001);
+        assertEquals(c.getPixelY(), 80, 0.00001);
 
     }
 }
